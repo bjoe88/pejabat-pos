becc23f6a43a74348171f1b0174c51b5e2620d55
Muhammad Azizol Aminuddin <Bjoe88@gmail.com>
2020-01-24 04:55:01 +0800

    Update request header

--- src/app/shared/components/main-panel/main-panel.component.ts
+++ src/app/shared/components/main-panel/main-panel.component.ts
@@ -1,15 +1,87 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faTrash, faToggleOff, faToggleOn } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-main-panel',
   templateUrl: './main-panel.component.html',
   styleUrls: ['./main-panel.component.scss']
 })
 export class MainPanelComponent implements OnInit {
+  requestHeaderButtonClassData = {
+    header: {
+      button: true,
+      selected: true
+    },
+    params: {
+      button: true,
+      selected: false
+    },
+    body: {
+      button: true,
+      selected: false
+    },
+    cookie: {
+      button: true,
+      selected: false
+    }
+  };
+  requestSelectedHeader = 'header';
+  trash = faTrash;
+  toggleOn = faToggleOn;
+  toggleOff = faToggleOff;
+  data = {
+    request: {
+      method: 'get',
+      url: 'https://wallet.iflix.com',
+      header: [{
+        enable: true,
+        key: 'Key1',
+        value: 'Value2'
+      },
+      {}],
+      params: [{
+        enable: true,
+        key: '',
+        value: ''
+      }],
+      body: [{
+        enable: true,
+        key: '',
+        value: ''
+      }],
+      cookie: [{
+        enable: true,
+        key: '',
+        value: ''
+      }],
+    },
+    response: {}
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  pickHeader(header:string) {
+    this.deselectAllHeader();
+    this.requestHeaderButtonClassData[header].selected = true;
+    this.requestSelectedHeader = header;
+  }
+
+  deselectAllHeader() {
+    this.requestHeaderButtonClassData.header.selected = false;
+    this.requestHeaderButtonClassData.params.selected = false;
+    this.requestHeaderButtonClassData.body.selected = false;
+    this.requestHeaderButtonClassData.cookie.selected = false;
+  }
+
+  requestHeaderButtonClass(header: string) {
+    return this.requestHeaderButtonClassData[header];
+  }
+  getRequestData() {
+    return this.data.request[this.requestSelectedHeader];
   }
 
 }
